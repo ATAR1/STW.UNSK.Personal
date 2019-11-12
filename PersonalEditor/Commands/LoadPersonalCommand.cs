5a8de566442c0045f1bb68fd328a471b8930d4ed
ATAR1 <thexenosmilu@gmail.com>
2019-11-12 15:52:45 +0500

    1

---
+++ PersonalEditor/Commands/LoadPersonalCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PersonalEditor.Repositories;
+
+namespace PersonalEditor.Commands
+{
+    public class LoadPersonalCommand
+    {
+        private IPersonalRepository repository;
+
+        public LoadPersonalCommand(IPersonalRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
---
+++ STW.UNSK.Personal.Model/DbModelContainerPartial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace STW.UNSK.Personal.Model
+{
+    public partial class DbModelContainer
+    {
+        public DbModelContainer(string connectionString):base(connectionString)
+        {
+
+        }
+    }
+}
